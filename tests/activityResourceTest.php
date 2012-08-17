283a0982593464a159016ff31e0980f1b20aab8d
Ian W. Remmel <design@ianwremmel.com>
2012-08-17 12:00:04 -0400

    added additional tests (some incomplete) for activityResource

--- tests/activityResourceTest.php
+++ tests/activityResourceTest.php
@@ -4,10 +4,32 @@ require_once 'lib/resource/activity.php';
 require_once 'config.php';
 
 class ActivityResourceTest extends PHPUnit_Framework_TestCase {
+	public function testCreate() {
+		$this->markTestIncomplete();
+	}
+
+	public function testRetrieve() {
+		$this->markTestIncomplete();
+	}
+
 	public function testRetrieveBulk() {
 		$ar = new ActivityResource();
 		$activities = $ar->retrieve();
 
 		$this->assertTrue(is_array($activities));
+	}
+
+	public function testUpdateAlwaysThrows() {
+		$ar = new ActivityResource();
+
+		$this->setExpectedException('BadMethodCallException');
+		$ar->update();
+	}
+
+	public function testDeleteAlwaysThrows() {
+		$ar = new ActivityResource();
+
+		$this->setExpectedException('BadMethodCallException');
+		$ar->delete();
 	}
 }
